e1ebbe470d7f4c626654f3d585180552eb433fd6
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-04-19 14:41:36 -0700

    Adds trip class and test file

---
+++ test/travelers-test.js
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+import Traveler from '../src/travelers'
+
+describe('traveler class', () => {
+
+	// beforeEach(() => {
+	// 	traveler = new Traveler ()
+	// })
+	it('should be a function', () => {
+		assert.isFunction(Traveler)
+	})
+
+})
---
+++ test/trips-test.js
@@ -0,0 +1,14 @@
+import { assert } from 'chai';
+import Trip from '../src/trips'
+
+describe('trip class', () => {
+
+	// beforeEach(() => {
+	// 	trip = new Trip()
+	// })
+
+	it('should be a function', () => {
+		assert.isFunction(Trip)
+	})
+
+})
