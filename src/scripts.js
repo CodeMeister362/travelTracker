54ba125638badd1ac2257770b8e075558d49b1e1
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-04-18 21:03:34 -0700

    Adds new png

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,7 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import './images/aiplane.png'
+import './images/airplane.png'
 
 
 console.log('This is the JavaScript entry file - your code begins here.');
