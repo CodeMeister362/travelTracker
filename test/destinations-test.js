1983a3367e32d60d3ca88c4cb0fcf6c8199a3528
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-04-19 14:58:18 -0700

    Adds destination class and test file.

---
+++ test/destinations-test.js
@@ -0,0 +1,13 @@
+import { assert } from 'chai';
+import Destination from '../src/destinations'
+
+describe('destination class', () => {
+
+	// beforeEach(() => {
+	// 	destination = new Destination ()
+	// })
+	it('should be a function', () => {
+		assert.isFunction(Destination)
+	})
+
+})
