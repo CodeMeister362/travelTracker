cb721db44fb6df28ee2a02ea0061a33f9c4497b6
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-04-18 23:12:52 -0700

    Adds hero image, book now btn, and some input fields

--- src/scripts.js
+++ src/scripts.js
@@ -6,7 +6,8 @@ import './css/styles.css';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-import 'src/images/airplane (1).png'
-
+import './images/airplane (2).png'
+import './images/alps.jpg'
+import './images/luggage.jpg'
 
 console.log('This is the JavaScript entry file - your code begins here.');
